2f3840413f7c2c0ce55de8333943fd9afd2a616d
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-06-23 02:35:27 -0700

    flash regions

--- fw/Core/Src/main.cpp
+++ fw/Core/Src/main.cpp
@@ -1,18 +1,29 @@
 #include "cmsis_os.h"
+#include "MetalDetector.hpp"
 
 extern "C" {
   extern int main_cube(void);
 
   void StartDefaultTask(void *argument)
   {
-    {
-      const osThreadAttr_t defaultTask_attributes = {
-        .name = "mainTask",
-        .stack_size = 256 * 4,
-        .priority = (osPriority_t) osPriorityNormal1,
-      };
-      osThreadId_t id = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-    }
+    // {
+    //   const osThreadAttr_t defaultTask_attributes = {
+    //     .name = "mainTask",
+    //     .stack_size = 256 * 4,
+    //     .priority = (osPriority_t) osPriorityNormal1,
+    //   };
+    //   osThreadId_t id = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+    // }
+
+    void volatile * volatile keep_foo;
+    keep_foo = &ext_dac_dma_buf;
+    keep_foo = &adc1_dma_buf;
+    keep_foo = &adc2_dma_buf;
+    keep_foo = &adc3_dma_buf;
+    keep_foo = &int_dac1_dma_buf;
+    keep_foo = &int_dac2_dma_buf;
+    keep_foo = &usart3_tx_dma_buf;
+    keep_foo = &usart3_rx_dma_buf;
 
     for(;;)
     {
@@ -22,16 +33,6 @@ extern "C" {
 }
 
 #include <cstdint>
-
-alignas(32) __attribute__((section(".user_ahb_sram_d2"))) uint16_t ext_dac_dma_buf[1024]; // AKA SPI2 TX buf
-alignas(32) __attribute__((section(".user_ahb_sram_d1"))) volatile uint16_t adc1_dma_buf[1024];
-alignas(32) __attribute__((section(".user_ahb_sram_d1"))) volatile uint16_t adc2_dma_buf[1024];
-alignas(32) __attribute__((section(".user_ahb_sram_d1"))) volatile uint16_t adc3_dma_buf[1024];
-alignas(32) __attribute__((section(".user_ahb_sram_d2"))) uint8_t int_dac1_dma_buf[1024];
-alignas(32) __attribute__((section(".user_ahb_sram_d2"))) uint8_t int_dac2_dma_buf[1024];
-
-alignas(32) __attribute__((section(".user_ahb_sram_d3"))) uint8_t usart3_tx_dma_buf[256];
-alignas(32) __attribute__((section(".user_ahb_sram_d3"))) volatile uint8_t  usart3_rx_dma_buf[256];
 
 int main(void)
 {
